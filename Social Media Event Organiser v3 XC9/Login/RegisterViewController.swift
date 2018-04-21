e06b171fe7e532e4210cf47f3c70821d8efd983c
seenashafai <seenas@btinternet.com>
2018-04-21 09:51:58 +0100

    Update table view

--- Social Media Event Organiser v3 XC9/Login/RegisterViewController.swift
+++ Social Media Event Organiser v3 XC9/Login/RegisterViewController.swift
@@ -10,6 +10,53 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func signUpAction(_ sender: Any)
+    {
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error != nil{
+                print(error as Any)
+                self.failureAlert()
+                print("firebase validation error")
+            }
+            else
+            {
+                print("success")
+                self.successAlert()
+            }
+        }
+
+    }
+    
+    func successAlert() {
+        
+        let alertController = UIAlertController(title: "Alert", message: "Registration Successful", preferredStyle: .alert)
+        
+        let action1 = UIAlertAction(title: "Continue to Sign In", style: .default) { (action:UIAlertAction) in
+            self.navigationController?.popToRootViewController(animated: true);
+        }
+        
+        alertController.addAction(action1)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func failureAlert() {
+        
+        let alertController = UIAlertController(title: "Alert", message: "Registration Failed\nPlease check your email and password", preferredStyle: .alert)
+        
+        let action1 = UIAlertAction(title: "Cancel", style: .default) { (action:UIAlertAction) in
+            print("failed");
+        }
+        
+        alertController.addAction(action1)
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
