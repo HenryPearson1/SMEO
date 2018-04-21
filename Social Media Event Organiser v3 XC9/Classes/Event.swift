8339e19c927e15051c96b2e1d51042f535b6cd13
Henry Pearson <henrypearson@Henrys-MacBook-Pro-2.local>
2018-04-21 09:36:13 +0100

    actually added events

---
+++ Social Media Event Organiser v3 XC9/Classes/Event.swift
@@ -0,0 +1,19 @@
+//
+//  eventStruct.swift
+//  Social Media Event Organiser v3 XC9
+//
+//  Created by Cosimo Lewis on 21/04/2018.
+//  Copyright © 2018 Seena Shafai. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+    
+    var name: String
+    var description: String
+    var location: String 
+    var time: String
+    var going = [User]()
+    
+}
