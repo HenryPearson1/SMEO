232665f0c69399160c6fca3281b17cab158d1cf8
grphine <grphine@gmail.com>
2018-04-25 20:42:22 +0100

    added view users functionality

--- Social Media Event Organiser v3 XC9/Friends/AddFriendTableViewController.swift
+++ Social Media Event Organiser v3 XC9/Friends/AddFriendTableViewController.swift
@@ -9,22 +9,26 @@
 import UIKit
 import Firebase
 
-class AddFriendTableViewController: UITableViewController {
-
+class AddFriendTableViewController: UITableViewController, UISearchResultsUpdating {
+    
+    var tag = ""
+    var filteredUserNames: [String]?
+    let searchController = UISearchController(searchResultsController: nil)
+    
     var userIDArray = [String]()
     var userSelected: String = ""
     
     var ref: DatabaseReference?
     var refHandle: DatabaseHandle?
-
+    
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
+        print("A")
         ref = Database.database().reference()
         
         let userIDListRef = ref?.child("UserList")
         refHandle = userIDListRef?.observe(.childAdded, with: {(snapshot) in
+            
             
             if let item = snapshot.value as? String
             {
@@ -32,85 +36,147 @@ class AddFriendTableViewController: UITableViewController {
                 self.userIDArray.append(item)
                 self.tableView.reloadData()
                 print("array", self.userIDArray)
-
+                
             }
+            
         })
         
-
-    }
+        print("B")
+        filteredUserNames = userIDArray
+        searchController.searchResultsUpdater = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.dimsBackgroundDuringPresentation = false
+        tableView.tableHeaderView = searchController.searchBar
         
+        
+        super.viewDidLoad()
+        print("C")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        searchController.dismiss(animated: false, completion: nil)
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        guard let usernames = filteredUserNames else {
+            return 0
+        }
+        return usernames.count
+        
     }
-
     
-    
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Prototype", for: indexPath)
+        
+        if let usernamesArray = filteredUserNames?.sorted() {
+            let cellName = ("\(String(describing: usernamesArray[indexPath.row]))")
+            cell.textLabel?.text = cellName
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+            filteredUserNames = userIDArray.filter { individual in
+                return individual.lowercased().contains(searchText.lowercased())
+                //puts everything in same case for effective searching
+            }
+            
+        } else {
+            filteredUserNames = userIDArray
+            //since the tableview only displayed what has been searched (filtered), if there are no searches, filtered = all
+        }
+        tableView.reloadData()
+        //reloads tableview with new data given by searching
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Users"
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tag = filteredUserNames![indexPath.row]
+        
+        print(tag)
+        //performSegue(withIdentifier: "ViewSegue", sender: nil)
+        /////////
+        //first gets patient name from filtered by the row the user selected
+        //then tag is the name from the global names with the index of the name that was selected
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    override func viewWillAppear(_ animated: Bool) {
+        filteredUserNames = userIDArray.sorted()
+        super.viewWillAppear(true)
+        self.tableView.reloadData()
+        
+        print("E")
+        
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        let toSend = segue.destination as! FriendDetailViewController
+        toSend.datacomingin = tag
     }
-    */
-
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
